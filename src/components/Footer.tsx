124f192a4de69142dbef5de31ae70393bd2e23db
ProdiCole777 <colefox.95@icloud.com>
2025-08-28 15:47:49 -0400

    updated images in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,27 +1,49 @@
+// src/components/Footer.tsx
+import html5 from "../assets/icons/html5.svg";
+import css from "../assets/icons/css.svg";
+import js from "../assets/icons/javascript.svg";
+import ts from "../assets/icons/typescript.svg";
+import react from "../assets/icons/react.svg";
+import nodejs from "../assets/icons/nodedotjs.svg";
+
+import python from "../assets/icons/python.svg";
+import vue from "../assets/icons/vuedotjs.svg";
+import angular from "../assets/icons/angular.svg";
+import ios from "../assets/icons/ios.svg";
+import android from "../assets/icons/android.svg";
+import nextjs from "../assets/icons/nextdotjs.svg";
+import django from "../assets/icons/django.svg";
+import mysql from "../assets/icons/mysql.svg";
+import cpp from "../assets/icons/cplusplus.svg";
+import csharp from "../assets/icons/sharp.svg";
+import c from "../assets/icons/c.svg";
+import php from "../assets/icons/php.svg";
+import dotnet from "../assets/icons/dotnet.svg";
+
 const Footer = () => {
   const learned = [
-    { name: "HTML", icon: "/src/assets/icons//html5.svg" },
-    { name: "CSS", icon: "/src/assets/icons/css.svg" },
-    { name: "JavaScript", icon: "/src/assets/icons/javascript.svg" },
-    { name: "TypeScript", icon: "/src/assets/icons/typescript.svg" },
-    { name: "React", icon: "/src/assets/icons/react.svg" },
-    { name: "Node.js", icon: "/src/assets/icons/nodedotjs.svg" },
+    { name: "HTML", icon: html5 },
+    { name: "CSS", icon: css },
+    { name: "JavaScript", icon: js },
+    { name: "TypeScript", icon: ts },
+    { name: "React", icon: react },
+    { name: "Node.js", icon: nodejs },
   ];
 
   const learning = [
-    { name: "Python", icon: "/src/assets/icons/python.svg" },
-    { name: "Vue", icon: "/src/assets/icons/vuedotjs.svg" },
-    { name: "Angular", icon: "/src/assets/icons/angular.svg" },
-    { name: "iOS", icon: "/src/assets/icons/ios.svg" },
-    { name: "Android", icon: "/src/assets/icons/android.svg" },
-    { name: "Next.js", icon: "/src/assets/icons/nextdotjs.svg" },
-    { name: "Django", icon: "/src/assets/icons/django.svg" },
-    { name: "SQL", icon: "/src/assets/icons/mysql.svg" },
-    { name: "C++", icon: "/src/assets/icons/cplusplus.svg" },
-    { name: "C#", icon: "/src/assets/icons/sharp.svg" },
-    { name: "C", icon: "/src/assets/icons/c.svg" },
-    { name: "PHP", icon: "/src/assets/icons/php.svg" },
-    { name: ".NET", icon: "/src/assets/icons/dotnet.svg" },
+    { name: "Python", icon: python },
+    { name: "Vue", icon: vue },
+    { name: "Angular", icon: angular },
+    { name: "iOS", icon: ios },
+    { name: "Android", icon: android },
+    { name: "Next.js", icon: nextjs },
+    { name: "Django", icon: django },
+    { name: "SQL", icon: mysql },
+    { name: "C++", icon: cpp },
+    { name: "C#", icon: csharp },
+    { name: "C", icon: c },
+    { name: "PHP", icon: php },
+    { name: ".NET", icon: dotnet },
   ];
 
   return (
@@ -29,9 +51,7 @@ const Footer = () => {
       <div className="max-w-6xl mx-auto flex flex-col md:grid md:grid-cols-3 gap-6 items-start">
         {/* Copyright */}
         <div className="text-center md:text-left">
-          <p>
-            &copy; {new Date().getFullYear()} Cole Fox. All rights reserved.
-          </p>
+          <p>&copy; {new Date().getFullYear()} Cole Fox. All rights reserved.</p>
         </div>
 
         {/* Skills Learned */}
@@ -43,11 +63,7 @@ const Footer = () => {
                 key={i}
                 className="flex items-center gap-1 bg-teal-600 px-2 py-1 rounded text-sm md:transition md:transform md:hover:scale-110 md:hover:bg-teal-500 cursor-default"
               >
-                <img
-                  src={tech.icon}
-                  alt={tech.name}
-                  className="hidden md:inline w-4 h-4"
-                />
+                <img src={tech.icon} alt={tech.name} className="hidden md:inline w-4 h-4" />
                 {tech.name}
               </span>
             ))}
@@ -63,11 +79,7 @@ const Footer = () => {
                 key={i}
                 className="flex items-center gap-1 bg-teal-800 px-2 py-1 rounded text-sm md:transition md:transform md:hover:scale-110 md:hover:bg-teal-700 cursor-default"
               >
-                <img
-                  src={tech.icon}
-                  alt={tech.name}
-                  className="hidden md:inline w-4 h-4"
-                />
+                <img src={tech.icon} alt={tech.name} className="hidden md:inline w-4 h-4" />
                 {tech.name}
               </span>
             ))}
